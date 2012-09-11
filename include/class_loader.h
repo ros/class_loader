e621e623044316e83bd487b288c9ec792d4618b9
Mirza Shah <mshah@willowgarage.com>
2012-09-11 13:05:53 -0700

    Added ref counting for load unload for multithread case.

--- include/class_loader.h
+++ include/class_loader.h
@@ -18,24 +18,6 @@ namespace plugins
  */
 class ClassLoader
 {
-
-  private:
-    template <class Base>    
-    void onPluginDeletion(Base* obj)
-    {
-      std::cout << "Calling onPluginDeletion() for obj ptr = " << obj << std::endl;
-      if(obj)
-      {
-        boost::mutex::scoped_lock lock(plugin_ref_count_mutex_);
-
-        delete(obj);
-        plugin_ref_count_ = plugin_ref_count_ - 1;
-        assert(plugin_ref_count_ >= 0);
-        if(plugin_ref_count_ == 0 && isOnDemandLoadUnloadEnabled())
-          unloadLibrary();
-      }
-    }
-
   public:
     /**
      * @brief  Constructor for ClassLoader
@@ -63,7 +45,7 @@ class ClassLoader
     std::string getLibraryPath(){return(library_path_);}
 
     /**
-     * @brief  Generates an instance of loadable classes (i.e. plugins)
+     * @brief  Generates an instance of loadable classes (i.e. plugins). It is not necessary for the user to call loadLibrary() as it will be invoked automatically if the library is not yet loaded (which typically happens when in "On Demand Load/Unload" mode).
      * @param  derived_class_name The name of the class we want to create (@see getAvailableClasses())
      * @return A boost::shared_ptr<Base> to newly created plugin object
      */
@@ -84,7 +66,7 @@ class ClassLoader
     }
 
     /**
-     * @brief  Indicates if a library is loaded within the scope of this ClassLoader. Note that the library may already be loaded internally through another ClassLoader, but until loadLibrary() method is called, the ClassLoader cannot create objects from said library. If we want to see if the library has been opened by somebody else, @see isLibraryLoadedByExternalObject()
+     * @brief  Indicates if a library is loaded within the scope of this ClassLoader. Note that the library may already be loaded internally through another ClassLoader, but until loadLibrary() method is called, the ClassLoader cannot create objects from said library. If we want to see if the library has been opened by somebody else, @see isLibraryLoadedByAnyClassloader()
      * @param  library_path The path to the library to load
      * @return true if library is loaded within this ClassLoader object's scope, otherwise false
      */
@@ -102,21 +84,50 @@ class ClassLoader
     bool isOnDemandLoadUnloadEnabled(){return(enable_ondemand_loadunload_);}
 
      /**
-     * @brief  Attempts to load a library on behalf of the ClassLoader. If the library is already opened, this method has no effect. If the library has been already opened by some other entity (i.e. another ClassLoader or global interface), this object is given permissions to access any plugin classes loaded by that other entity.
+     * @brief  Attempts to load a library on behalf of the ClassLoader. If the library is already opened, this method has no effect. If the library has been already opened by some other entity (i.e. another ClassLoader or global interface), this object is given permissions to access any plugin classes loaded by that other entity. This is
      * @param  library_path The path to the library to load
      */
     void loadLibrary();
 
      /**
-     * @brief  Attempts to unload a library loaded within scope of the ClassLoader. If the library is not opened, this method has no effect. If the library is opened by other entities (i.e. another ClassLoader or global interface), the library will NOT be unloaded internally -- however this ClassLoader will no longer be able to instantiate plugins bound to that library.
+     * @brief  Attempts to unload a library loaded within scope of the ClassLoader. If the library is not opened, this method has no effect. If the library is opened by other entities (i.e. another ClassLoader or global interface), the library will NOT be unloaded internally -- however this ClassLoader will no longer be able to instantiate plugins bound to that library. If there are plugin objects that exist in memory created by this classloader, a warning message will appear and the library will not be unloaded. If loadLibrary() was called multiple times (e.g. in the case of multiple threads or purposefully in a single thread), the user is responsible for calling unloadLibrary() the same number of times. The library will not be unloaded within the context of this classloader until the number of unload calls matches the number of loads.
      */
     void unloadLibrary();
 
+  private:
+    /**
+     * @brief Callback method when a plugin created by this class loader is destroyed
+     * @param obj - A pointer to the deleted object
+     */
+    template <class Base>    
+    void onPluginDeletion(Base* obj)
+    {
+      std::cout << "Calling onPluginDeletion() for obj ptr = " << obj << std::endl;
+      if(obj)
+      {
+        std::cout << "Attempting to acquire lock...";
+        boost::mutex::scoped_lock lock(plugin_ref_count_mutex_);
+        std::cout << "got it." << std::endl;
+        delete(obj);
+        plugin_ref_count_ = plugin_ref_count_ - 1;
+        assert(plugin_ref_count_ >= 0);
+        if(plugin_ref_count_ == 0 && isOnDemandLoadUnloadEnabled())
+          unloadLibraryInternal(false);
+      }
+    }
+
+  /**
+   * @brief As the library may be unloaded in "on-demand load/unload" mode, unload maybe called from createInstance(). The problem is that createInstance() locks the plugin_ref_count as does unloadLibrary(). This method is the implementation of unloadLibrary but with a parameter to decide if plugin_ref_mutex_ should be locked
+   * @param lock_plugin_ref_count - Set to true if plugin_ref_count_mutex_ should be locked, else false
+   */
+  void unloadLibraryInternal(bool lock_plugin_ref_count);
 
   private:
-    bool        enable_ondemand_loadunload_;
-    std::string library_path_;  
-    int         plugin_ref_count_;
+    bool enable_ondemand_loadunload_;
+    std::string library_path_;
+    int load_ref_count_;  
+    boost::mutex load_ref_count_mutex_;
+    int plugin_ref_count_;
     boost::mutex plugin_ref_count_mutex_;
 };
 
--- src/class_loader.cpp
+++ src/class_loader.cpp
@@ -7,6 +7,7 @@ namespace plugins
 ClassLoader::ClassLoader(const std::string& library_path, bool enable_ondemand_loadunload) :
 enable_ondemand_loadunload_(enable_ondemand_loadunload),
 library_path_(library_path),
+load_ref_count_(0),
 plugin_ref_count_(0)
 {
   if(!isOnDemandLoadUnloadEnabled())
@@ -31,15 +32,36 @@ bool ClassLoader::isLibraryLoadedByAnyClassloader()
 
 void ClassLoader::loadLibrary()
 {
+  boost::mutex::scoped_lock lock(load_ref_count_mutex_);
+  load_ref_count_ = load_ref_count_ + 1;
   plugins::plugins_private::loadLibrary(getLibraryPath(), this);
 }
 
 void ClassLoader::unloadLibrary()
 {
+  unloadLibraryInternal(true);
+}
+
+void ClassLoader::unloadLibraryInternal(bool lock_plugin_ref_count)
+{
+  boost::mutex::scoped_lock load_ref_lock(load_ref_count_mutex_);
+  if(lock_plugin_ref_count)
+    plugin_ref_count_mutex_.lock(); //Can't use scoped lock, remember to unlock...
+
   if(plugin_ref_count_ > 0)
     std::cout << "ClassLoader: MAJOR WARNING!!! Attempting to unload library while objects created by this loader exist in the heap! You should delete your objects before attempting to unload the library or destroying the ClassLoader. The library will NOT be unloaded." << std::endl;
   else
-    plugins::plugins_private::unloadLibrary(getLibraryPath(), this);
+  {
+    load_ref_count_ = load_ref_count_ - 1;
+    if(load_ref_count_ == 0)
+      plugins::plugins_private::unloadLibrary(getLibraryPath(), this);
+    else if(load_ref_count_ < 0)
+      load_ref_count_ = 0;
+  }
+
+  if(lock_plugin_ref_count)
+    plugin_ref_count_mutex_.unlock(); //...and this is where we unlock
+ 
 }
 
 }
