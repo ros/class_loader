bd1762d713de626a81e31fb89aaff5af9232fe3d
William Woodall <william@osrfoundation.org>
2015-10-26 12:03:00 -0700

    add another integration test

---
+++ test/fviz_case_study/fviz.cpp
@@ -0,0 +1,8 @@
+#include "fviz.h"
+
+#include <cstdio>
+
+void foo(std::string msg)
+{
+  printf("From foo(): %s\n", msg.c_str());
+}
---
+++ test/fviz_case_study/fviz_plugin_base.h
@@ -0,0 +1,10 @@
+#ifndef FVIZ_PLUGIN_BASE_H
+#define FVIZ_PLUGIN_BASE_H
+
+class FvizPluginBase
+{
+public:
+  virtual void speak() = 0;
+};
+
+#endif  // FVIZ_PLUGIN_BASE_H
