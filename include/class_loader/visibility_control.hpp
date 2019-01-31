2b37f643c6e7980da0583c26814704028464c078
James Xu <kejxu@microsoft.com>
2019-01-31 09:08:52 -0800

    add documentation links to compiler warnings

--- include/class_loader/visibility_control.hpp
+++ include/class_loader/visibility_control.hpp
@@ -64,7 +64,9 @@
 // based on wiki.ros.org: http://wiki.ros.org/win_ros/Contributing/Dll%20Exports
 // Ignore warnings about import/exports when deriving from std classes.
 #ifdef _MSC_VER
+  // https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-1-c4251?view=vs-2017
   #pragma warning(disable: 4251)
+  // https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-2-c4275?view=vs-2017
   #pragma warning(disable: 4275)
 #endif
 
