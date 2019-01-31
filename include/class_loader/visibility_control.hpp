0ee528194867d707a2e33f2fa0f15ba6f7190bbf
James Xu <kejia.xu@microsoft.com>
2019-01-31 10:07:28 -0800

    Ignore warnings about import/exports when deriving from std classes on MSVC (#116)
    * ignore dll import and export errors on msvc (#11)
    
    * add documentation links to compiler warnings
    
    * Update include/class_loader/visibility_control.hpp
    
    Co-Authored-By: kejxu <kejia.xu@microsoft.com>
    
    * remove duplicate links

--- include/class_loader/visibility_control.hpp
+++ include/class_loader/visibility_control.hpp
@@ -61,4 +61,13 @@
   #define CLASS_LOADER_PUBLIC_TYPE
 #endif
 
+// based on wiki.ros.org: http://wiki.ros.org/win_ros/Contributing/Dll%20Exports
+// Ignore warnings about import/exports when deriving from std classes.
+// https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-1-c4251
+// https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-2-c4275
+#ifdef _MSC_VER
+  #pragma warning(disable: 4251)
+  #pragma warning(disable: 4275)
+#endif
+
 #endif  // CLASS_LOADER__VISIBILITY_CONTROL_HPP_
